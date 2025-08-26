0f158b2d7ba09f2703f2cdae5f9d47be955ebfbf
jrwilliamson <157505268+QuirkyPlague@users.noreply.github.com>
2025-08-25 22:34:58 -0400

    fix maybe?

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -10,7 +10,6 @@ const bool colortex0MipmapEnabled = true;
 const bool colortex2MipmapEnabled = true;
 */
 
-const bool shadowHardwareFiltering = true;
 
 #define SHADOW_SAMPLES 8 //[4 8 12 16 20 24 28 32]
 #define SHADOW_SOFTNESS 1.0 //[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0]
